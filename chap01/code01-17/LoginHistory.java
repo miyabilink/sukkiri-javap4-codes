ad0758eb0bddc4c21580c21488562841f531186c
Gakuto OE <ohe@miyabilink.jp>
2024-02-04 18:45:23 +0900

    第2章コードの追加

--- chap01/code01-17/LoginHistory.java
+++ chap01/code01-17/LoginHistory.java
@@ -1,5 +1,5 @@
 public record LoginHistory(String user, int retry, boolean result) { 
-  public LoginHistory() {
+  public LoginHistory {
     if (retry < 0) throw new IllegalArgumentException();
   }
 }
